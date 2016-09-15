6d0b3667e6eba0a68919c965d53f8f5c9f06b702
Te Riu Warren <teriu@teriu.name>
2016-09-15 15:12:42 +1200

    Fix webpack helper

--- public/index.php
+++ public/index.php
@@ -1,36 +1,4 @@
-<?php
-if (! function_exists('webpack')) {
-    /**
-     * Get the path to an asset file handled with Webpack.
-     *
-     * @param  string  $file
-     * @param  string  $buildDirectory
-     * @param  string  $devUrl
-     * @return string
-     *
-     * @throws \InvalidArgumentException
-     */
-    function webpack($file, $buildDirectory = 'public/assets/builds', $devUrl = 'http://localhost:3000/static')
-    {
-        static $manifest;
-        static $manifestPath;
-
-        if (is_null($manifest) || $manifestPath !== $buildDirectory) {
-            // Check manifest file exists
-            if (file_exists($buildDirectory.'/manifest.json')) {
-                $manifest = json_decode(file_get_contents($buildDirectory.'/manifest.json'), true);
-                $manifestPath = $buildDirectory;
-            } else {
-                return $devUrl.'/'.$file;
-            }
-        }
-
-        if (isset($manifest[$file])) {
-            return '/'.trim($buildDirectory.'/'.$manifest[$file], '/');
-        }
-    }
-}
-?>
+<?php require_once('../helpers.php'); ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
     <head>
